d13983ce52c2a55a8ab2e8c2c5b24f35eb80c979
Dor Rondel <dor.rondel@gmail.com>
2016-05-18 21:12:52 -0400

    Restructured folder for Fall 2016 retake :(

--- Week1/labs/labClass.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  cout << "Hello, world!" << endl; //I prefer \n ....
-  return 0;
-}
--- week3/lecture/wed.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  //learnt about ternary operators. Like in js, for example:
-  cout << (1 == 1 ? "Equal" : "Not Equal") << endl;
-}
